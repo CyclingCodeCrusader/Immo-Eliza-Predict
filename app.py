48c0fe7738b7f7a21e57bc60e8dfc6ad688278b5
Tom <tom_vanbelle@hotmail.com>
2024-11-08 08:49:50 +0100

    20241108 testrender

--- app.py
+++ app.py
@@ -10,10 +10,34 @@ app = FastAPI(port=PORT)
 @app.get("/")
 async def root():
     """Route that returns 'Alive!' if the server runs."""
-    return {"Status": "Alive!"}
+    return {"Status": "Alive! Welcome !"}
 
+@app.get("/multi/{value}")
+async def multiply(value: int):
+    return {"result": value * 2}
+# Defining path operation for /name endpoint
+
+@app.get('/{name}')
+def hello_name(name : str): 
+    # Defining a function that takes only string as input and output the
+    # following message. 
+    return {'message': f'Welcome to GeeksforGeeks!, {name}'}
 
 @app.get("/hello")
 async def say_hello(user: str = "Anonymous"):
     """Route that will return 'hello {user}'."""
     return {"Message": f"Hello {user}!"}
+
+# Define a Pydantic model for the expected JSON structure
+from pydantic import BaseModel
+class SalaryData(BaseModel):
+    salary: int
+    bonus: int
+    taxes: int
+
+# Endpoint using Pydantic model
+@app.post("/salary")
+async def results(data: SalaryData):
+    # The data is automatically validated and parsed into `SalaryData` instance
+    result = data.salary + data.bonus - data.taxes
+    return {"message": "Data received successfully", "input": data, "result": result}
