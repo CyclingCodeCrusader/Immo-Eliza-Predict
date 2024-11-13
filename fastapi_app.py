64d370be9ecae0d3f8dc8a410b7282c681654e72
Tom <tom_vanbelle@hotmail.com>
2024-11-13 23:28:34 +0100

    20241113 cleaned modeller

--- fastapi_app.py
+++ fastapi_app.py
@@ -18,7 +18,6 @@ class SelectionModel(BaseModel):
     net_habitable_surface: float
     facade_count : int
     land_surface: float
-    #has_assigned_city_10 : bool
     kitchen_type_ord_enc : int
     building_condition_ord_enc : int
     epc_ord_enc : int
@@ -35,13 +34,12 @@ def receive_selection(selection: SelectionModel):
             selection.bedroom_count, 
             selection.net_habitable_surface,
             selection.facade_count, 
-            selection.land_surface, 
-            #selection.has_assigned_city_10, 
+            selection.land_surface,
             selection.kitchen_type_ord_enc, 
             selection.building_condition_ord_enc, 
             selection.epc_ord_enc,
             selection.locality_code
-    ]]
+            ]]
     print(type(test_data), test_data)
 
     # Call methods from the Processor class and return a df
@@ -63,76 +61,9 @@ def hello_name(name : str):
     # following message. 
     return {'message': f'Welcome to testapi via POST!, {name}'}
 
-class province_input(BaseModel):
-    code : int
-    #province: int
-
 @app.post('/api/province')
 def province(input: province_input):
     code = input.code
     if code == 9000:
         name = "Gantoise"
     return {"message": "Data received successfully", "input": code, "result": name} #
-
-
-class predict_request_body(BaseModel):
-    bedroom_count : int
-    net_habitable_surface: float
-    facade_count : int
-    land_surface: float
-    has_assigned_city_10 : bool
-    kitchen_type_ord_enc : int
-    building_condition_ord_enc : int
-    epc_ord_enc : int
-
-
-@app.post('/predict')
-def predict(data : predict_request_body):
-
-    # Making the data in a form suitable for prediction
-    test_data = [[
-            data.bedroom_count, 
-            data.net_habitable_surface, 
-            data.facade_count,
-            data.land_surface, 
-            data.has_assigned_city_10, 
-            data.kitchen_type_ord_enc, 
-            data.building_condition_ord_enc, 
-            data.epc_ord_enc
-
-    ]]
-    # Extract the data from the request body
-    received_data = data.bedroom_count
-    print(f"Received data: {received_data}")  # For logging purposes
-
-    # Perform any processing here
-    #processed_data = received_data.upper()  # Example processing
-
-    # Return a response
-    return {"received": received_data}
-    
-    # Load the model from the file
-    #loaded_pipeline = joblib.load('best_model_pipeline.joblib')
-
-    # Create a new house 
-    #feature_names = ['bedroom_count', 'net_habitable_surface', 'facade_count', 'land_surface', 'has_assigned_city_10', 
-    #                'kitchen_type_ord_enc', 'building_condition_ord_enc', 'epc_ord_enc']
-    #, 
-    #                'province_Brabant_Wallon', 'province_Brussels', 'province_East_Flanders', 'province_Flemish_Brabant', 'province_Hainaut', 
-    #                'province_Liege', 'province_Limburg', 'province_Luxembourg', 'province_Namur', 'province_West_Flanders']
-
-    #new_house = np.array([3,200,4,500,1,3,3,3,0,0,1,0,0,0,0,0,0,0]).reshape(1,-1)
-    #new_house = np.array(test_data).reshape(1,-1)
-
-    #df_new_house= pd.DataFrame(new_house, columns=feature_names)
-    #print(df_new_house)
-
-    # Use the loaded model to make predictions
-    #predictions = loaded_pipeline.predict(df_new_house)
-    #print("Type of predictions: ", type(predictions))
-    # Save the predictions as csv file to have quick view
-    #np.savetxt("predictions.csv", predictions, delimiter=",", fmt="%.2f")
-
-    # Return the Result
-    #return { 'Price prediction in euro:' : round(predictions[0])}
-    return { "message": "Data received successfully", "input": test_data, "result": result}
